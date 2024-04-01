c09707912c96aa7f4937392bdaba6fc990ae5106
p1utoze <1ds21ai001@dsce.edu.in>
2024-04-01 15:03:43 +0530

    changes: moved requirements.txt to app; added dotenv loading in __init__.py; update pre-commit config file

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -1,8 +1,16 @@
 from st_supabase_connection import SupabaseConnection
-from .helpers import prepare_indian_geojson, prepare_indian_languages, prepare_unit_data
+from .helpers import (
+    prepare_indian_geojson,
+    prepare_indian_languages,
+    prepare_unit_data,
+)
+from dotenv import load_dotenv
+
+load_dotenv()
+
 __all__ = [
     "SupabaseConnection",
     "prepare_indian_geojson",
     "prepare_indian_languages",
-    "prepare_unit_data"
+    "prepare_unit_data",
 ]
