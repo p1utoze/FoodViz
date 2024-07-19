d5a7b9a7c7b1198195dbf68c4f867628e53e0c41
p1utoze <1ds21ai001@dsce.edu.in>
2024-07-20 00:05:18 +0530

    update: implemented new helper functions for the search menu

--- src/helpers.py
+++ src/helpers.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import geopandas as gpd
 import pandas as pd
-import numpy as np
+import os
+from llama_index.core import load_index_from_storage, StorageContext
+from llama_index.embeddings.voyageai import VoyageEmbedding
+from llama_index.vector_stores.faiss import FaissVectorStore
+from llama_index.core.retrievers import BaseRetriever
 from .config import PROJECT_ROOT
-
 
 @st.cache_data(show_spinner=":blue[Loading the database..] Please wait...", persist=True)
 def prepare_indian_geojson():
@@ -27,3 +30,30 @@ def prepare_unit_data(path: str, drop_col: str = "type") -> pd.DataFrame:
     if drop_col:
         df.drop("type", axis=1, inplace=True)
     return df
+
+def generate_color_range(start_rgb, end_rgb, steps):
+    """
+    Generate a color range between two RGB colors.
+
+    :param start_rgb: Tuple representing the starting RGB color.
+    :param end_rgb: Tuple representing the ending RGB color.
+    :param steps: Number of steps in the color range.
+    :return: List of interpolated RGB colors.
+    """
+    color_range = []
+    for t in range(steps + 1):  # +1 to ensure the end color is included
+        r = int(start_rgb[0] * (1 - t/steps) + end_rgb[0] * (t/steps))
+        g = int(start_rgb[1] * (1 - t/steps) + end_rgb[1] * (t/steps))
+        b = int(start_rgb[2] * (1 - t/steps) + end_rgb[2] * (t/steps))
+        color_range.append(f"rgb({r} {g} {b})")
+    return color_range
+
+@st.cache_resource
+def load_retriever(persist_dir: str, top_k: int = 5) -> BaseRetriever:
+    embed_model = VoyageEmbedding(model_name="voyage-large-2", voyage_api_key=os.environ["VOYAGE_API_KEY"])
+    storage_context = StorageContext.from_defaults(
+        vector_store=FaissVectorStore.from_persist_dir(persist_dir),
+        persist_dir=persist_dir,
+    )
+    loaded_index = load_index_from_storage(storage_context, embed_model=embed_model, show_progress=True)
+    return loaded_index.as_retriever(similarity_top_k=top_k)
