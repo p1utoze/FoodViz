bbde945e14c1108fc76baa590d1896445f49377e
p1utoze <1ds21ai001@dsce.edu.in>
2024-07-20 00:08:02 +0530

    init: implement a search engine queried from the Indian food database

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,6 +1,6 @@
 from comparer import run as comparer_run
 from home import run as home_run
 from viewer import run as viewer_run
+from search import run as search_run
 
-
-__all__ = ["home_run", "comparer_run", "viewer_run"]
+__all__ = ["home_run", "comparer_run", "viewer_run", "search_run"]
