b86cc3b1aa42d79897c9d998cb1341e7f8698d62
p1utoze <1ds21ai001@dsce.edu.in>
2024-10-14 11:42:31 +0530

    fix: responsive bubble svg for different viewports

--- foodviz/pages/home.py
+++ foodviz/pages/home.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
 import streamlit as st
 import streamlit.components.v1 as components
-from pathlib import Path
-from foodviz.utils.helpers import load_bubble_data
+from js2py.translators.friendly_nodes import js_postfix
+from streamlit_js_eval import streamlit_js_eval
+
 from foodviz.utils.config import PROJECT_ROOT
 
 custom_chart = Path(__file__).parent / "frontend" / "bubble.html"
@@ -9,12 +12,18 @@ chart_data = PROJECT_ROOT / "data" / "food-types.csv"
 
 def run():
     empty = st.empty()
+    # height = streamlit_js_eval(js_expressions='parent.window.innerHeight', key='SCR2')
+    width = streamlit_js_eval(js_expressions='window.innerWidth', key="SCR1")
+    height = 600
+    width = 800 if width > 800 else width
+    # height = streamlit_js_eval(js_expressions='parent.window.innerHeight', key="SCR")
+    # print(width
     with empty.container():
         col1, col2 = st.columns([1, 1])
         col1.markdown(
             """
-            <h1 style="font-size: 8em; font-weight: bold; color: #e63946;">FoodViz</h1>
-            <h3 style="font-size: 4em; color: #a8dadc;">Interactive Indian Food Composition Explorer</h3>
+            <h1 style="font-size: 8rem; font-weight: bold; color: #e63946;">FoodViz</h1>
+            <h3 style="font-size: 4rem; color: #a8dadc;">Interactive Indian Food Composition Explorer</h3>
             """,
             unsafe_allow_html=True,
         )
@@ -22,14 +31,15 @@ def run():
             canvas = f.read()
 
         with col2:
-            components.html(canvas, height=600, width=800)
+            # width, height = 800, 600
+            canvas = canvas.replace('svg width="800" height="600"', f'svg width="{width}" height="{height}"')
+            components.html(canvas, height=height, width=width)
         # col2.image(
         #     "https://img.freepik.com/premium-photo/different-types-meats-vegetables-fruits-lay-"
         #     "supermarkets-generative-ai_572887-4418.jpg",
         #     use_column_width=True,
         # )
 
-    st.divider()
 
     with st.container():
         cphrase = "Are you a Gym freak or a Health enthusiast?"
